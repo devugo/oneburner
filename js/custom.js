a7e3832c49c7542a3b4f725b9253a7c04d6c76f0
Ezenwankwo Ugochukwu <ugoezenwankwo@gmail.com>
2021-01-13 18:36:11 +0100

    Hero section

--- js/custom.js
+++ js/custom.js
@@ -76,3 +76,18 @@
 //     }
    
 // })
+
+// NAVBAR
+let elements = document.getElementsByClassName(' oneburner-dropdown');
+
+for(var i = 0; i<elements.length; i++){
+    let element = elements[i];
+    element.addEventListener('mouseenter', function(){
+        document.getElementById('oneburner-overlay').style.display = 'block';
+    })
+
+    element.addEventListener('mouseleave', function(){
+        document.getElementById('oneburner-overlay').style.display = 'none';
+    })
+}
+
